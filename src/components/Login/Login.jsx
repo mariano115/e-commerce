d9ae35dc24d42e875343214c127626ee19e39663
mariano115 <you@example.com>
2020-12-20 22:45:21 -0300

    1

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,22 @@
+import './Login.css';
+
+import React, {useState} from 'react';
+
+function Login() {
+    const adminUser = {
+        email: "",
+        password:""
+    }
+
+    /*const [user, setUser] = useState({name:"", password:""})
+    const [error, setError] = useState({name:"", password:""})*/
+
+
+    return <div className="root">
+        <form>
+            <h1>Hola</h1>
+        </form>
+    </div>
+}
+
+export default Login();
---
+++ src/views/login/Login.jsx
@@ -0,0 +1,8 @@
+const LoginComponent = require('../../components/Login/Login');
+
+function LoginPage() {
+    return <div className="login"><LoginComponent/></div>
+        
+}
+
+export default LoginPage
